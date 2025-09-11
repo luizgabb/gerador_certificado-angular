971ad6a94a0f0be55bb04a6d541d92a6498feec3
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-09-11 16:54:32 -0300

    começo navbar component

---
+++ src/app/_components/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar implements OnInit {
+  ngOnInit(): void {
+    this.mensagem()
+  }
+
+
+  mensagem() {
+    console.log("Jesus salva")
+  }
+}
