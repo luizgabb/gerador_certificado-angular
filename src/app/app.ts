3a6cb1c4b5a4f8c43051108ad888cbff9c648400
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-09-15 13:30:58 -0300

    add these components: secondary button, primary button, item and interface base

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
