611e3470d0ae210e54f5a13c33f40e13c7c853ed
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-09-19 13:37:14 -0300

    validando os campos do formulario

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
+  nome:string = ""
+  atividade: string = ""
+  atividades: string[] = []
 
+  campoValido(control: NgModel) {
+    return control.invalid && control.touched
+  }
 }
